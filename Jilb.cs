901435134fa6ccceb617645f51fcba6753412720
Denis Shafarenko <phisika001@mail.ru>
2023-04-17 23:59:45 +0300

    Initial commit and C-group is done

---
+++ Jilb.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    public static class Jilb
+    {
+        public static int CloseBracketInd(string str, int begin, string open, out bool noBreak)
+        {
+            string close;
+            noBreak = false;
+            switch (open)
+            {
+                case "(":
+                    close = ")";
+                    break;
+                case "{":
+                    close = "}";
+                    break;
+                case "case":
+                    close = "break";
+                    break;
+                default:
+                    return -1;
+            }
+            int nestLvl = 1;
+            int i = begin;
+            while (nestLvl != 0)
+            {
+                i++;
+                if (str.Length - i >= "switch".Length && str.Substring(i, "switch".Length).Equals("switch"))
+                {
+                    while (str[i] != '{') i++;
+                    i = CloseBracketInd(str, i, "{", out noBreak);
+                }
+                else if (str.Length - i >= open.Length && str.Substring(i, open.Length).Equals(open))
+                {
+                    if (open.Equals("case"))
+                    {
+                        nestLvl--;
+                        noBreak = true;
+                    }
+                    else
+                    {
+                        nestLvl++;
+                    }
+                }
+                else if (str.Length - i >= close.Length && str.Substring(i, close.Length).Equals(close))
+                    nestLvl--;
+                else if (str.Length <= i)
+                {
+                    noBreak = true;
+                    return i - 1;
+                }
+            }
+            return i;
+        }
+        public static int MaxNestLvl(string code)
+        {
+            int maxNestLvl = -1, nestLvl = -1;
+            bool noBreak;
+            for (int i = 0; i < code.Length; i++)
+            {
+                if (code[i] == '\"')
+                {
+                    i++;
+                    while (!(code[i] == '\"' && code[i - 1] != '\\')) i++;
+                }
+                if ((code.Length - i >= "if".Length && code.Substring(i, "if".Length).Equals("if") && (code[i + "if".Length] == ' ' || code[i + "if".Length] == '(')) ||
+                    (code.Length - i >= "else".Length && code.Substring(i, "else".Length).Equals("else") && (code[i + "else".Length] == ' ' || code[i + "else".Length] == '{')) ||
+                    (code.Length - i >= "while".Length && code.Substring(i, "while".Length).Equals("while") && (code[i + "while".Length] == ' ' || code[i + "while".Length] == '{')) ||
+                    (code.Length - i >= "for".Length && code.Substring(i, "for".Length).Equals("for") && (code[i + "for".Length] == ' ' || code[i + "for".Length] == '{')))
+                {
+                    if (code.Substring(i, "if".Length) == "if" ||
+                        code.Substring(i, "while".Length) == "while" ||
+                        code.Substring(i, "for".Length) == "for")
+                    {
+                        while (code[i] != '(') i++;
+                        i = CloseBracketInd(code, i, "(", out noBreak);
+                    }
+                    while (code[i] != '{') i++;
+                    nestLvl = 1 + MaxNestLvl(code.Substring(i + 1, CloseBracketInd(code, i, "{", out noBreak) - (i + 1)));
+                    i = CloseBracketInd(code, i, "{", out noBreak) + 1;
+                    if (nestLvl > maxNestLvl)
+                        maxNestLvl = nestLvl;
+                    nestLvl = -1;
+                }
+                else if (code.Length - i >= "switch".Length && code.Substring(i, "switch".Length).Equals("switch") && (code[i + "switch".Length] == ' ' || code[i + "switch".Length] == '('))
+                {
+                    while (code[i] != '{') i++;
+                    int loop_end = CloseBracketInd(code, i, "{", out noBreak);
+                    while (i <= loop_end)
+                    {
+                        if (code.Length - i >= "case".Length && code.Substring(i, "case".Length).Equals("case") && (code[i + "case".Length] == ' ' || code[i + "case".Length] == '('))
+                        {
+                            int j;
+                            nestLvl++;
+                            i += "case".Length;
+                            j = CloseBracketInd(code, i, "case", out noBreak);
+                            int nestLvlTmp = nestLvl + MaxNestLvl(code.Substring(i + 1, j - i)) + 1;
+                            if (noBreak)
+                                nestLvl = nestLvlTmp;
+                            else
+                                i = j + "break".Length;
+                            if (nestLvlTmp > maxNestLvl)
+                                maxNestLvl = nestLvlTmp;
+                        }
+                        i++;
+                    }
+                    nestLvl = -1;
+                }
+            }
+            return maxNestLvl;
+        }
+    }
+    /*internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var reader = new StreamReader("D:\\Study\\Metra\\Задание№2_МетрикиПотокаУправления\\code_switch.mts");
+            string code;
+            try
+            {
+                code = reader.ReadToEnd();
+            }
+            finally
+            {
+                reader.Close();
+            }
+            Console.WriteLine(Jilb.MaxNestLvl(code));
+        }
+    }*/
+}
---
+++ Program.cs
@@ -0,0 +1,111 @@
+﻿using ConsoleApp1;
+using System.Runtime.ExceptionServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Laba3Console
+{
+    internal class Program
+    {
+        public static readonly HashSet<string> reservedNames = new HashSet<string>{ "function", "number", "let", "return",
+            "const", "for", "while", "if", "else", "switch", "case", "default", "break" };
+        public static readonly string identifierTemplate = @"(?<name>[a-zA-Z_][a-zA-Z0-9_]*)(?![\w(])";
+        enum Group
+        {
+            P = 1,
+            M = 2,
+            T = 3,
+            C = 3
+        }
+        class Variable
+        {
+            public Group group;
+            public bool used;
+            public Variable(Group group, bool used = false)
+            {
+                this.group = group;
+                this.used = used;
+            }
+        }
+
+        public static HashSet<string> GetIdentifiers(string code)
+        {
+            Regex regex = new Regex(identifierTemplate);
+            MatchCollection matches = regex.Matches(code);
+            var result = new HashSet<string>();
+
+            foreach (Match match in matches)
+            {
+                if (!reservedNames.Contains(match.Value))
+                {
+                    result.Add(match.Value);
+                }
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            FileStream? fstream = null;
+            string? code;
+            try
+            {
+                fstream = new FileStream(@"D:\Study\Metra\Задание№3_МетрикиПотокаДанных\Laba3Console\Laba3Console\code.txt", FileMode.Open);
+                byte[] buffer = new byte[fstream.Length];
+                fstream.ReadAsync(buffer, 0, buffer.Length);
+                code = Encoding.Default.GetString(buffer);
+            }
+            finally
+            {
+                fstream?.Close();
+            }
+
+            Dictionary<string, Variable> variables = new Dictionary<string, Variable>();
+
+            Console.WriteLine(code.Length);
+
+            var var_C = new HashSet<string>();
+            for (int i = 0; i < code.Length; i++)
+            {
+                if (i < code.Length - " if".Length && code.Substring(i, " if".Length) == " if" ||
+                    i < code.Length - " while".Length && code.Substring(i, " while".Length) == " while" ||
+                    i < code.Length - " for".Length && code.Substring(i, " for".Length) == " for" ||
+                    i < code.Length - " switch".Length && code.Substring(i, " switch".Length) == " switch")
+                {
+                    while (code[i] != '(') i++;
+                    bool noBreak;
+                    int closeBracket = Jilb.CloseBracketInd(code, i, "(", out noBreak);
+                    var_C.UnionWith(GetIdentifiers(code.Substring(i + 1, closeBracket - i - 1)));
+                } 
+                else if (i < code.Length - " case".Length && code.Substring(i, " case".Length) == " case")
+                {
+                    int j = i;
+                    while (code[j] != ':') j++; 
+                    var_C.UnionWith(GetIdentifiers(code.Substring(i + 1, j - i - 1)));
+                }
+            }
+
+            foreach (var var in var_C) 
+            {
+                Console.WriteLine(var);
+            }
+
+            /*Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Regex regex = new Regex(identifierTemplate);
+            MatchCollection matches = regex.Matches("i == 1");
+            if (matches.Count > 0)
+            {
+                foreach (Match match in matches)
+                    Console.WriteLine(match.Groups["name"].Value);
+            }
+            else
+            {
+                Console.WriteLine("Совпадений не найдено");
+                Console.WriteLine(Regex.IsMatch("", identifierTemplate));
+            }*/
+        }
+    }
+}
